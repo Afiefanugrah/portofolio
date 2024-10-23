adbec83256c4e7f305a6ae052b0b786fcd9dc6fe
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-10-23 21:20:05 +0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid d-flex justify-content-between">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary w-100">
+      <div class="container">
         <a class="navbar-brand" href="#">M. Afief Anugrah</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav ms-auto">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Blog</a>
+            <a class="nav-link active me-3" aria-current="page" href="#">Home</a>
+            <a class="nav-link me-3" href="#">Blog</a>
             <a class="nav-link" href="#">Project</a>
             {/* <a class="nav-link disabled" aria-disabled="true">Disabled</a> */}
           </div>
