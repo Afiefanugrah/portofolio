757b70c577ee0dc722485343b57fdb0c9376a769
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-10-23 21:19:48 +0700

    home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import { Container } from "react-bootstrap"
 import Navbar from "./Navbar"
+import "../style/LandingPage.css"
+import "../style/NavbarPage.css"
 
 const Home = () => {
   return (
-    <Container>
-      <main className="">
+    <Container fluid className="p-0 bg-success bg-my">
+      <main className="bg-main">
         <Navbar />
+        <div class="container text-center border my-container">
+          <div class="row align-items-center">
+            <div class="col-6">Column</div>
+            <div class="col-6">Column</div>
+          </div>
+        </div>
         <div>Home</div>
       </main>
       
